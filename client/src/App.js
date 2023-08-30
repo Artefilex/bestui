da35a190f4c0228aff235e47c8684da69c35fa26
Artefilex <baris.tncdmr@gmail.com>
2023-08-30 03:02:03 +0300

    sidebar nav için biçimlendirme yapıldı

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
-import "./App.css";
-
 import Sidebar from "./mainComponent/nav/Sidebar";
 import Location from "./mainComponent/nav/Location";
 
 function App(){
   return (
     <div className="App">
-       <Sidebar/>
-      <Location/>
-    </div>
+      <Sidebar/>
+      <div className="location">
+      <Location/> 
+      </div>
+  </div>
   );
 }
 
--- client/src/mainComponent/nav/Sidebar.jsx
+++ client/src/mainComponent/nav/Sidebar.jsx
@@ -1,19 +1,63 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "../../assest/css/sidebar.css"
 import { Link } from "react-router-dom"
 import logo from "../../assest/img/logo.png"
+import {GrHomeRounded} from "react-icons/gr"
+import {AiOutlineMenu} from "react-icons/ai"
 function Sidebar() {
+  const [show ,setShow] = useState(false)
+  const [mobile,setMobile] = useState(window.innerWidth <500)
+  const handleClick = useCallback(() =>{
+    setShow(!show)
+  },[show])
+
+  useEffect(() => {
+    const handleResize = () => {
+      const newMobile = window.innerWidth < 500;
+      setMobile(newMobile);
+      setShow(false)
+      if (!newMobile) {
+        setShow(true);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return <div className="Sidebar flex" >
-    <div>
+    <div className="sidebar-logo">
         <Link to="/">  
            <img src={logo} alt="logo" />
         </Link>
     </div> 
-   <nav> 
-   <Link to="/" >  
-      <img src={logo} alt="logo" />
-    </Link>
-   </nav>
+   {mobile && 
+     <button className="menu-button" onClick={handleClick}> <AiOutlineMenu/> </button>
+    }
+     {(show || !mobile) && (
+        <nav className="flex sidebar-nav"> 
+        <Link to="/" >  
+          <GrHomeRounded className="navbar-icon"/>
+         </Link>
+         <Link to="/" >  
+           <span>Buttons</span>
+         </Link>
+         <Link to="/" >  
+             <span>Forms</span> 
+         </Link>
+         <Link to="/" >  
+             <span>Cards</span> 
+         </Link>
+         <Link to="/" >  
+             <span>animation</span> 
+         </Link>
+         <Link to="/" >  
+             <span>Css</span> 
+         </Link>
+        </nav>
+      )}
+   
+   
   </div>;
 }
 
